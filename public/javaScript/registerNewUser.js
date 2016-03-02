dc528980cd09a8a49abd38354aba834dc55ea4ed
Marie <mariessa@stud.ntnu.no>
2016-03-02 11:24:38 +0100

    checkUsername

--- public/javaScript/registerNewUser.js
+++ public/javaScript/registerNewUser.js
@@ -2,8 +2,19 @@
  * Created by Marie on 02.03.2016.
  */
 
+var usernameRegex = /^[a-zA-Z0-9]+$/;
+var name;
+
+function checkUsername(){
+    name = document.getElementById("reg_username").value;
+    var validUsername = name.value.match(usernameRegex);
+    if(validUsername == null){
+        alert("Your first name is not valid. Only characters A-Z, a-z and '-' are  acceptable.");
+        name.focus();
+        return false;
+    }
+}
 
 function registerUser() {
-    var name = document.getElementById("reg_username").value;
     alert(name);
 }
