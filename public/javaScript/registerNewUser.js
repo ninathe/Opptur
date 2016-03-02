216d30489325719528436ee5f6600ab3c00bd3e2
Marie <mariessa@stud.ntnu.no>
2016-03-02 13:36:59 +0100

    changes to registerUser()

--- public/javaScript/registerNewUser.js
+++ public/javaScript/registerNewUser.js
@@ -1,55 +1,61 @@
 
-var usernameRegex = /^[a-zA-Z0-9]+$/;
-var name;
+var USERNAME_REGEX = /^[a-zA-Z0-9]+$/;
+var PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+var username;
+var password;
+var passwordConfirm;
+
 //Validates the username
 function checkUsername(){
-    name = document.getElementById("reg_username").value;
-    var validUsername = name.value.match(usernameRegex);
-    if(validUsername == null){
-        alert("Your first username is not valid. Only characters and digits are acceptable.");
-        name.focus();
-        return false;
+    username = document.getElementById("reg_username").value;
+    var validUsername = USERNAME_REGEX.test(username);
+
+    //var validUsername = username.value.match(USERNAME_REGEX);
+    //if(validUsername == null){     FØRSTE FORSØKS GREIE
+    //    alert("Your username is not valid. Only characters and digits are acceptable.");
+    //    username.focus();
+    //    return false;
+    //}
+    if(username == "") {
+        alert("Error: Username cannot be blank!");
+        username.focus();
+        //return false;
     }
-    return true
+    if(!validUsername) {
+        alert("Error: Username must contain only letters, numbers and underscores!");
+        username.focus();
+        //return false;
+    }
+    //return true
 }
 
 //validate password
-
-function checkPassword(str)
-{
-    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-    return re.test(str);
+password = document.getElementById("reg_password").value;
+passwordConfirm = document.getElementById("reg_password_confirm").value;
+function checkPass(str) {
+    return PASSWORD_REGEX.test(str);
 }
 
-function checkForm(form)
-{
-    if(form.username.value == "") {
-        alert("Error: Username cannot be blank!");
-        form.username.focus();
-        return false;
-    }
-    re = /^\w+$/;
-    if(!re.test(form.username.value)) {
-        alert("Error: Username must contain only letters, numbers and underscores!");
-        form.username.focus();
-        return false;
-    }
-    if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
-        if(!checkPassword(form.pwd1.value)) {
+function checkPassword() {
+    if(password != "" && password == passwordConfirm) {
+        if(!checkPass(password)) {
             alert("The password you have entered is not valid!");
-            form.pwd1.focus();
-            return false;
+            password.focus();
+            //return false;
         }
     } else {
         alert("Error: Please check that you've entered and confirmed your password!");
-        form.pwd1.focus();
-        return false;
+        password.focus();
+        //return false;
     }
-    return true;
+    //return true;
 }
 
 
 //Checks that all input values are valid and if so add it to the database
-function registerUser() {
+function registerUser(){
 //lag kode for å lagre informasjon om brukeren i databasen
+//    if(checkUsername()){
+
+    //}
 }
