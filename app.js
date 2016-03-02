1777aa146b5a76cfaf3435c3b24294ccf6202a5d
Marie <mariessa@stud.ntnu.no>
2016-03-02 09:34:10 +0100

    gjort noen kall til databasen - ikke ferdig
    Var selvfølgelig inne i master og ikke brach så håper dette går bra

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser=require('cookie-parser');
 var bodyParser=require('body-parser');
 var mongoose =require('mongoose');
 //var passport=require('passport');
+var MongoClient = require('mongodb').MongoClient;
 
 //varUser=require('./models/user.js');
 
@@ -41,16 +42,66 @@ if(app.get('env')==='development'){
 
 }
 
+ //StandardURIformat:mongodb://[dbuser:dbpassword@]host:port/dbname
+ var uri="mongodb://heroku_dvldc026:bkgs9p6shbfhl0ub46j51gk9b@ds015398.mongolab.com:15398/heroku_dvldc026";
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect(uri, function(err, db) {
+  if (err) {
+    return console.dir(err);
+  }
+  console.log("Vi er tilkoblet databasen");
+  var collection = db.collection('test');
+  //collection.drop();
+
+
+  });
 
 module.exports=app;
 
+
 /*
+  var doc1 = {'brukernavn':'doc1', 'passerod' : 'sdffd'};
+  var doc2 = {'hello':'doc2'};
+  var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+  //
+  collection.insert(doc1);
+  //
+  collection.insert(doc2, {w:1}, function(err, result) {});
+  //
+  collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+  //var collection = db.collection('test');
+  //var doc = {mykey:1, fieldtoupdate:1};
+  //
+  //collection.remove();
 
- //StandardURIformat:mongodb://[dbuser:dbpassword@]host:port/dbname
+  var collection = db.collection('test');
+  var docs = [{mykey:1}, {mykey:2}, {mykey:3}];
 
- varuri='mongodb://heroku_dvldc026:bkgs9p6shbfhl0ub46j51gk9b@ds015398.mongolab.com:15398/heroku_dvldc026';
+  collection.insert(docs, {w:1}, function(err, result) {
+
+    //collection.find().toArray(function(err, items) {});
+
+    var stream = collection.find({mykey:{$ne:2}}).stream();
+    stream.on("data", function(item) {});
+    stream.on("end", function() {});
+
+    //collection.findOne({mykey:1}, function(err, item) {});
+
+    return console.toLocaleString(stream);
+  });
+
+ var collection2 = db.collection('user'){
+ _id:String, //email
+ userName :String,
+ password :String
+ });
 
  mongodb.MongoClient.connect(uri,function(err,db){
+
 
  if(err){
  console.log("unabletoconnecttoMongoDBserverError...",err);
@@ -59,11 +110,6 @@ module.exports=app;
  //inserttherestofthedatabsequerieshere
 
  }
- /!*varcollection=db.collection({
- _id:String,//email
- userName:String,
- password:String
- });
 
  varuser=mongodb.model('emp',collection);*!/
  /!*
