bb2cb5d90c3d0e5c30bf6866360618d2689d2c26
Nina Tandrevold Hermanrud <nina.th.93@gmail.com>
2016-03-11 15:51:37 +0100

    Endringer css
    Div endringer på css — sidemeny

--- public/javaScript/login.js
+++ public/javaScript/login.js
@@ -1,12 +1,12 @@
 $(document).ready(
     function() {
-        $(".user").focusin(function(){
+        $("#user").focusin(function(){
           $(".inputUserIcon").css("color", "#ffa44c");
         }).focusout(function(){
           $(".inputUserIcon").css("color", "white");
         });
 
-        $(".pass").focusin(function(){
+        $("#pass").focusin(function(){
           $(".inputPassIcon").css("color", "#ffa44c");
         }).focusout(function(){
           $(".inputPassIcon").css("color", "white");
