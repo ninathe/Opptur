025bdd891906ed2d14a04ccdb766c854d1dde814
Marie <mariessa@stud.ntnu.no>
2016-04-08 11:30:38 +0200

    login henter bruker fra databasen!

--- public/javaScript/findUser.js
+++ public/javaScript/findUser.js
@@ -1,15 +1,22 @@
 
+//Talking to frontEnd
 
 function logIn(){
-    var email           = document.getElementById("user").value;
-    var password        = document.getElementById("pass").value;
+    var email= document.getElementById("user").value;
+    var password = document.getElementById("pass").value;
 
 
 
-    var dbUser = new User; // TODO: må bruke den User-collectionen fra DB
-//finds the user with the email ****, selecting the name and password
-    dbUser.findOne({ 'email': email, 'password' : password }, 'nickname password', function (err, user) {
-        if (err) return handleError(err);
-        alert('%s has the email %s with the password %s.', user.nickname, user.email, user.password ) // Noe has the email noe@hotmail.com with the password 123
-    });
+    //Get info from front end  (user), and send the info to back end so it can check database
+    $.post("/logIn",
+        {
+            "email" : email,
+            "password": password
+        }
+    ).complete( function(data,status){
+        alert("complete");
+        alert(status.statusCode);
+        alert(data);
+
+    })
 }
