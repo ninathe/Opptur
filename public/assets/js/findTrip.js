5add600680c4f07b2605df888312cfeff01d5e02
Nina Tandrevold Hermanrud <nina.th.93@gmail.com>
2016-05-24 17:38:07 +0200

    Added files to master

---
+++ public/assets/js/findTrip.js
@@ -0,0 +1,36 @@
+
+function findTrip() {  //is activated when the "sig up" button is clicked
+    var duration = document.getElementById("distanceTripValue").value;
+
+
+    //Plassering, avstand til punktet, må bruke kortestevei greier
+    //var plassering = document.getElementById("startPos").value;
+    //var distanceToPoint = document.getElementById("inputDistanceToStart").value;
+
+
+
+    //Get info from front end  (user), and send the info to back end so it can check database
+    $.post("/findTrip",
+        {
+            "duration" : duration,
+        }
+    ).complete( function(data,status){
+        console.log("Data loaded: " + data + "\nStatus: " + status);
+
+    })
+
+}
+
+
+//CONTROLLING EVENTS IN jQuery
+$(document).ready(function(){
+
+    //LOADING newUser
+    //Click the button event!
+    $(document).on('click', '#btnSearch', function(){
+        alert("Longitude: " + longitude + " Latitude: " + latitude + " Avstand til Startpnkt: "
+            + distanceValue.value + " Lengde på tur: " + distanceTripValue.value + " Vanskelighetsgrad: " + difficult.value);
+    });
+
+
+});
