62d4c04ebef0f6bc552a2c5e8ddf8e5a128def5a
Nina Tandrevold Hermanrud <nina.th.93@gmail.com>
2016-05-24 17:32:04 +0200

    Aner ikke

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express     = require('express');
 var path        = require('path');
-var logger      = require('morgan');
+var logger      = require('morgan');2
 var cookieParser= require('cookie-parser');
 var bodyParser  = require('body-parser');
 var mongoose    = require('mongoose');
